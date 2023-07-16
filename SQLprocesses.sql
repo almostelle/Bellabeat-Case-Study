c5d15ff52b8a31fecda4527c4eece42ee6e65c1e
almostelle <78129610+almostelle@users.noreply.github.com>
2023-07-15 19:25:39 -0500

    Update SQLprocesses.sql

--- SQLprocesses.sql
+++ SQLprocesses.sql
@@ -1,12 +1,17 @@
+/* Which users are the most active (by the criteria: how many minutes did they log)? */
+/* Counts the active days, sums the different types of minutes the user logged. The table is grouped by each user's entries*/
 SELECT Id, COUNT(Id) as number_of_activedays, SUM(SedentaryMinutes) as sum_of_sedentaryminutes, SUM(LightlyActiveMinutes) as sum_of_lightminutes, SUM(FairlyActiveMinutes) as sum_of_fairlyminutes, SUM(VeryActiveMinutes) as sum_of_veryminutes, SUM(LightActiveDistance) as sum_of_lightdistance, SUM(ModeratelyActiveDistance) as sum_of_moderatedistance, SUM(VeryActiveDistance) as sum_of_verydistance
 FROM `bellabeat-case-study-384718.DailyActivity.daily_intensities` 
 GROUP BY Id 
 ORDER BY number_of_activedays 
 LIMIT 1000
-
+  
+/*Which days of the week were users most active?*/
+/* Formats the dates in "YYYY-MM-DD" format to days of the week as string format */
 SELECT id,ActivityDate, CAST((ActivityDate) AS STRING FORMAT "DAY") AS date_to_day 
 FROM `bellabeat-case-study-384718.DailyActivity.daily_activity`
-
+  
+/*How does one user record their activities compared to the other users?*/
 /*Creates a table the combines daily activity records, weight records, and sleep records*/
 CREATE TABLE DailyActivity.joined_activity_weight_sleep AS
 SELECT daily_Activity.Id,CAST(daily_sleep_2.Id AS INTEGER) AS ID_sleep,weight_merged.Id AS Id_weight, daily_activity.ActivityDate, SleepDay, weight_merged.ActivityDate AS ActivityDate_weight, TotalSleepRecords, TotalDistance, TrackerDistance, LoggedActivitiesDistance, VeryActiveDistance, ModeratelyActiveDistance, LightActiveDistance, SedentaryActiveDistance TotalTimeInBed, SedentaryMinutes, FairlyActiveMinutes, LightlyActiveMinutes, VeryActiveMinutes, WeightKg, WeightPounds, Fat, BMI , Calories
@@ -15,7 +20,14 @@ FROM(DailyActivity.weight_merged
   ON weight_merged.ActivityDate = daily_activity.ActivityDate AND weight_merged.Id = daily_activity.Id)
 FULL OUTER JOIN DailyActivity.daily_sleep_2 
 ON daily_sleep_2.SleepDay = daily_activity.ActivityDate AND daily_sleep_2.Id = daily_activity.Id
-
+  
+/* This additional query found nulls in the above table. If there were nulls, it meant that there was a record in the cohesive table that wasn't in the separate weight or sleep table. The output was empty, as expected */
+SELECT Id, ActivityDate
+FROM `bellabeat-case-study-384718.DailyActivity.joined_activity_weight_sleep`
+WHERE Id IS NULL
+LIMIT 1000
+  
+/* How many days did each user log their sleep or weight? What is the percentage of those days for each user?*/
 /* Create table that returns Ids, logged days, count of sleep / weight records, and the percentage of those records vs. the users logged days */
 CREATE TABLE DailyActivity.counts_sums_activity AS
 SELECT Id,SumDistance, logged_days,without_sleep_records, with_sleep_records, without_weight_records, with_weight_records, without_cal_records, with_calorie_records, sum_of_sedentaryminutes, sum_of_lightminutes, sum_of_veryminutes, (with_sleep_records/logged_days)*100 AS perc_sleep, (with_weight_records/logged_days)*100 AS perc_weight, (with_calorie_records/logged_days)*100 AS perc_calories 
@@ -38,7 +50,8 @@ with_weight_records,
   FROM DailyActivity.joined_activity_weight_sleep
   GROUP BY Id
   ORDER BY logged_days)
-
+  
+/*At what time during a 24-hour period did the users log their steps. When did they log the most steps? The least steps? */
 /* Creates table that separates and sums steps by the time of day and groups by Id
 CREATE TABLE DailyActivity.steps AS */
 SELECT Id, SUM(Steps) as total_steps,COUNT(DISTINCT(ActivityDate)) AS days_logged,
@@ -47,13 +60,15 @@ SUM(CASE WHEN time_of_day = 'Midday' THEN Steps ELSE 0 END) AS steps_midday,
 SUM(CASE WHEN time_of_day = 'Night' THEN Steps ELSE 0 END) AS steps_night,
 SUM(CASE WHEN time_of_day = 'LateNight' THEN Steps ELSE 0 END) AS steps_latenight,
 FROM
-(SELECT Id, ActivityDate,Steps,
+  /* Uses CASE to separate the 24-hour times into four sections*/
+(SELECT Id, ActivityDate,Steps, 
 CASE
 WHEN day_interval BETWEEN '06:00:00' AND '12:00:00' THEN 'Morning'
   WHEN day_interval BETWEEN '12:00:01' AND '18:00:00' THEN 'Midday'
   WHEN day_interval BETWEEN '18:00:01' AND '22:00:00' THEN 'Night'
   ELSE 'LateNight' END AS time_of_day
 FROM(
+/* Converts the datetime into 24-hour times*/
 SELECT Id, CAST((ActivityMinute) AS DATE) AS ActivityDate,Steps, TIME(TIMESTAMP (ActivityMinute)) AS day_interval
 FROM DailyActivity.minute_steps))
 GROUP BY Id
